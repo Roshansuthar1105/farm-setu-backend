3a4bba67b88049b54165d059df12f69c72de401a
roshansuthar1105 <roshansuthar2023@gmail.com>
2024-11-27 11:19:07 +0530

    products route updated

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -16,6 +16,16 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Error getting products' }); 
     }
 });
+router.get('/:seller', async (req, res) => {
+    try {
+        const {seller}=req.params;
+        const products = await Product.find({ "seller":seller });
+        res.json(products);
+    } catch (error) {
+        console.error('Error in getProducts:', error);
+        res.status(500).json({ error: 'Error getting products' }); 
+    }
+});
 router.get('/:productId', async (req, res) => {
     try {
         const {productId} = req.params;
@@ -40,13 +50,37 @@ router.post('/addproduct', async (req, res) => {
         const newProduct = new Product({ name, price, description, image, category ,seller });
         await newProduct.save();
         
-        res.status(201).json({ message: 'Product added successfully' });
+        res.status(201).json({ message: 'Product added successfully',id:newProduct._id });
     } catch (error) {
         console.error('Error in addProduct:', error);
         res.status(500).json({ error: 'Error adding product' }); 
     }
 });
-
-
+router.delete('/delete/:productId', async (req, res) => {
+    try {
+        const {productId} = req.params;
+        const product = await Product.findByIdAndDelete(productId);
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        console.error('Error in deleteProduct:', error);
+        res.status(500).json({ error: 'Error deleting product' });
+    }
+});
+router.patch('/update/:productId', async (req, res) => {
+    try {
+        const {productId} = req.params;
+        const product = await Product.findByIdAndUpdate(productId, req.body, {new: true});
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.json({message:"Product updated !",product:product});
+    } catch (error) {
+        console.error('Error in updateProduct:', error);
+        res.status(500).json({ error: 'Error updating product' });
+    }
+});
 
 export default router;
